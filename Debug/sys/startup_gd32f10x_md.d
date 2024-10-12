f4cc4d9b1f69c8e319c9a95eb6500edc92359cc0
Кирилл Караваев <karavayk@bk.ru>
2024-10-12 14:47:54 +0300

    Changed CLK mux. and source(10Mhz quartz)

--- Debug/sys/startup_gd32f10x_md.d
+++
@@ -1,1 +0,0 @@
-sys/startup_gd32f10x_md.o: ../sys/startup_gd32f10x_md.s
--- Debug/sys/subdir.mk
+++ Debug/sys/subdir.mk
@@ -4,39 +4,27 @@
 ################################################################################
 
 # Add inputs and outputs from these tool invocations to the build variables 
-S_SRCS += \
-../sys/startup_gd32f10x_md.s 
-
 C_SRCS += \
 ../sys/stm32_it.c \
-../sys/system_gd32f10x.c \
 ../sys/system_stm32f10x.c 
 
 S_UPPER_SRCS += \
 ../sys/startup_stm32f10x_md.S 
 
 OBJS += \
-./sys/startup_gd32f10x_md.o \
 ./sys/startup_stm32f10x_md.o \
 ./sys/stm32_it.o \
-./sys/system_gd32f10x.o \
 ./sys/system_stm32f10x.o 
-
-S_DEPS += \
-./sys/startup_gd32f10x_md.d 
 
 S_UPPER_DEPS += \
 ./sys/startup_stm32f10x_md.d 
 
 C_DEPS += \
 ./sys/stm32_it.d \
-./sys/system_gd32f10x.d \
 ./sys/system_stm32f10x.d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
-sys/%.o: ../sys/%.s sys/subdir.mk
-	arm-none-eabi-gcc -mcpu=cortex-m3 -g3 -c -x assembler-with-cpp -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@" "$<"
 sys/%.o: ../sys/%.S sys/subdir.mk
 	arm-none-eabi-gcc -mcpu=cortex-m3 -g3 -c -x assembler-with-cpp -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@" "$<"
 sys/%.o sys/%.su sys/%.cyclo: ../sys/%.c sys/subdir.mk
@@ -45,7 +33,7 @@ sys/%.o sys/%.su sys/%.cyclo: ../sys/%.c sys/subdir.mk
 clean: clean-sys
 
 clean-sys:
-	-$(RM) ./sys/startup_gd32f10x_md.d ./sys/startup_gd32f10x_md.o ./sys/startup_stm32f10x_md.d ./sys/startup_stm32f10x_md.o ./sys/stm32_it.cyclo ./sys/stm32_it.d ./sys/stm32_it.o ./sys/stm32_it.su ./sys/system_gd32f10x.cyclo ./sys/system_gd32f10x.d ./sys/system_gd32f10x.o ./sys/system_gd32f10x.su ./sys/system_stm32f10x.cyclo ./sys/system_stm32f10x.d ./sys/system_stm32f10x.o ./sys/system_stm32f10x.su
+	-$(RM) ./sys/startup_stm32f10x_md.d ./sys/startup_stm32f10x_md.o ./sys/stm32_it.cyclo ./sys/stm32_it.d ./sys/stm32_it.o ./sys/stm32_it.su ./sys/system_stm32f10x.cyclo ./sys/system_stm32f10x.d ./sys/system_stm32f10x.o ./sys/system_stm32f10x.su
 
 .PHONY: clean-sys
 
